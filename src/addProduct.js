44ed866221089c27fe654daf8654bcfcc6323228
emmy2397 <jana.bethke@mail.de>
2021-10-29 14:23:07 +0200

    reentered dropdown input

--- src/addProduct.js
+++ src/addProduct.js
@@ -138,6 +138,34 @@ render() {
                         <button className="submitButton">Submit</button>
                     </div>
 
+                    <div class="formBox tab2" style={this.state.tab ===2 ? {display : "block"}: {display: "none"}}>
+                        <h2>Step 2</h2>
+                            <label htmlFor="name">Price:</label>
+                            <input type="number" id="price" onChange={this.handleChange}/>
+
+                            <label htmlFor="name">Urgency:</label>
+                            <select name="urgency" id="urgency" onChange={this.handleChange}>
+                            {/*Both select inputs have a default  value "not selected" because it is not displaying the top option (value) otherwise */}
+                                <option value="Not selected">- Not selected -</option>
+                                <option value="High">High</option>
+                                <option value="Moderate">Moderate</option>
+                                <option value="Low">Low</option>
+                            </select>
+                            <br/>
+                            <label htmlFor="name">Usage:</label>
+                            <select name="usage" id="usage" onChange={this.handleChange}>
+                                <option value="Not selected">- Not selected -</option>
+                                <option value="Daily">Daily</option>
+                                <option value="Few times per week">Few times per week</option>
+                                <option value="Weekly">Weekly</option>
+                                <option value="Every few weeks">Every few weeks</option>
+                                <option value="Monthly">Monthly</option>
+                                <option value="More rarely">More rarely</option>
+                            </select>
+                            <br/><br/>
+                            <button class="submitButton">Submit</button>
+                    </div>
+
                     <div className="formBox tab3" style={this.state.tab === 3 ? {display : "block"}: {display : "none"}}>
                         <h2>Step 4</h2>
                     
